9367e5d60f141689121fa496480f5b8a10add168
hj <hj89kim@gmail.com>
2021-03-08 00:33:26 +0900

    first commit

---
+++ 1.crawling.py
@@ -0,0 +1,77 @@
+# https://github.com/affjljoo3581/canrevan
+
+# TRAIN DATA
+# 정치 canrevan --category 100 --start_date 20200501 --end_date 20200531 --max_page 5
+# IT과학 canrevan --category 105 --start_date 20200501 --end_date 20200531 --max_page 5
+# 사회 canrevan --category 102 --start_date 20200501 --end_date 20200531 --max_page 5
+
+# TEST DATA
+# 정치 canrevan --category 100 --start_date 20190501 --end_date 20190531 --max_page 5 --output_path './asd.txt'
+# IT과학 canrevan --category 105 --start_date 20190501 --end_date 20190531 --max_page 5 --output_path 과학1년
+# 사회 canrevan --category 102 --start_date 20190501 --end_date 20190531 --max_page 5
+
+
+# TRAIN DATA 2
+
+# TRAIN DATA
+# 정치 canrevan --category 100 --start_date 20200301 --end_date 20210301 --max_page 5 --output_path './asd.txt'
+# IT과학 canrevan --category 105 --start_date 20200301 --end_date 20210301 --max_page 5
+# 사회 canrevan --category 102 --start_date 20200301 --end_date 20210301 --max_page 5
+
+# TEST DATA
+# 정치 canrevan --category 100 --start_date 20180301 --end_date 20190301 --max_page 5
+# IT과학 canrevan --category 105 --start_date 20180301 --end_date 20190301 --max_page 5
+# 사회 canrevan --category 102 --start_date 20180301 --end_date 20190301 --max_page 5
+
+
+'''
+# 네이버 뉴스 id
+
+< li
+
+
+class ="lnb_menu_home on" > < a href="/main/home.nhn" class ="nclicks(LNB.home)" > < span class ="tx" > 뉴스홈 < / span > < span class ="blind" > 선택됨 < / span > < / a > < / li >
+
+< li > < a
+href = "/main/list.nhn?mode=LSD&mid=sec&sid1=001"
+
+
+class ="nclicks(LNB.flash)" > < span class ="tx" > 속보 < / span > < / a > < / li >
+
+< li > < a
+href = "/main/main.nhn?mode=LSD&mid=shm&sid1=100"
+
+
+class ="nclicks(LNB.pol)" > < span class ="tx" > 정치 < / span > < / a > < / li >
+
+< li > < a
+href = "/main/main.nhn?mode=LSD&mid=shm&sid1=101"
+
+
+class ="nclicks(LNB.eco)" > < span class ="tx" > 경제 < / span > < / a > < / li >
+
+< li > < a
+href = "/main/main.nhn?mode=LSD&mid=shm&sid1=102"
+
+
+class ="nclicks(LNB.soc)" > < span class ="tx" > 사회 < / span > < / a > < / li >
+
+< li > < a
+href = "/main/main.nhn?mode=LSD&mid=shm&sid1=103"
+
+
+class ="nclicks(LNB.lif)" > < span class ="tx" > 생활 / 문화 < / span > < / a > < / li >
+
+< li > < a
+href = "/main/main.nhn?mode=LSD&mid=shm&sid1=104"
+
+
+class ="nclicks(LNB.wor)" > < span class ="tx" > 세계 < / span > < / a > < / li >
+
+< li > < a
+href = "/main/main.nhn?mode=LSD&mid=shm&sid1=105"
+
+
+class ="nclicks(LNB.sci)" > < span class ="tx" > IT / 과학 < / span > < / a > < / li >
+
+'''
